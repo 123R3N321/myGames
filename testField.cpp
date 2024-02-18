c1594d52c2ea496741c385d749e7a1a4b9e0fe0e
123R3N321 <jr5887@nyu.edu>
2024-02-17 20:00:07 -0500

    Add disclaimer. Ready for submission 1

--- testField.cpp
+++ testField.cpp
@@ -1,3 +1,15 @@
+/**
+* Author: Ren
+* Assignment: Simple 2D Scene
+* Date due: 2024-02-17, 11:59pm
+* I pledge that I have completed this assignment without
+* collaborating with anyone else, in conformance with the
+* NYU School of Engineering Policies and Procedures on
+* Academic Misconduct.
+**/
+
+
+
 #define GL_SILENCE_DEPRECATION
 #define GL_GLEXT_PROTOTYPES 1
 #define STB_IMAGE_IMPLEMENTATION
